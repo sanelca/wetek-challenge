e26dbc690fe469d6fe83b4902aee12e0ec0f5e23
sanelca <sanel.hodzic.web@gmail.com>
2019-10-10 16:39:56 +0200

    Playing with routes

--- index.php
+++ index.php
@@ -7,3 +7,15 @@
  */
 
 require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function(){
+    echo "Home";
+});
+
+$app->get('/users', function(){
+    echo "Users";
+});
+
+$app->run();
